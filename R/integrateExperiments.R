2af6396013ce966363e9ee152346c6f769455a63
dosorio <dcosorioh@tamu.edu>
2020-06-25 15:30:35 -0500

    Update integrateExperiments.R

--- R/integrateExperiments.R
+++ R/integrateExperiments.R
@@ -1,11 +1,11 @@
-#' @importFrom harmony RunHarmony
+#' @import harmony
 #' @import Seurat
 integrateExperiments <- function(dataset){
   dataset <- Seurat::NormalizeData(dataset, verbose = FALSE)
   dataset <- suppressWarnings(Seurat::FindVariableFeatures(dataset, verbose = FALSE))
   dataset <- Seurat::ScaleData(dataset, features = Seurat::VariableFeatures(dataset), verbose = FALSE)
   dataset <- Seurat::RunPCA(dataset, verbose = FALSE, )
-  if(length(unique(Idents(dataset))) > 1){
+  if(length(unique(Seurat::Idents(dataset))) > 1){
     dataset <- suppressWarnings(harmony::RunHarmony(dataset, 'orig.ident', max.iter.harmony = 100, max.iter.cluster = 100))
     dataset <- RunTSNE(dataset, reduction = 'harmony', dims = 1:5, verbose= FALSE, check_duplicates= FALSE)
     dataset <- suppressWarnings(RunUMAP(dataset, reduction = 'harmony', dims = 1:5, verbose= FALSE))
